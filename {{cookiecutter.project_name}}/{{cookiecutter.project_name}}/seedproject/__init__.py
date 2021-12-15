6d0bd82809350d40533cc5145b4bfafa7c2257cc
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-15 15:37:01 -0500

    Cookiefy more values

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/seedproject/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Seed module doc"""
-
-__descr__ = "seeddescription"
-__version__ = "seedversion"
-__license__ = "seedlicense"
-__author__ = u"seedauthor"
-__author_email__ = "seed@email"
-__copyright__ = u"seedcopyright seedauthor"
-__url__ = "seedurl"
