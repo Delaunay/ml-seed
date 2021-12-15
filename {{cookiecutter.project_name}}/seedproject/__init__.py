f7835983458a6f6e29ff37f0b628a7d71d52f9e8
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-15 15:22:09 -0500

    Remove duplicated folders

--- {{cookiecutter.project_name}}/seedproject/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Seed module doc"""
-
-__descr__ = "{{cookiecutter.author}}"
-__version__ = "{{cookiecutter.version}}"
-__license__ = "{{cookiecutter.license}}"
-__author__ = u"seedauthor"
-__author_email__ = "{{cookiecutter.email}}"
-__copyright__ = u"{{cookiecutter.copyright}} seedauthor"
-__url__ = "{{cookiecutter.url}}"
